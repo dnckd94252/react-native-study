aff88df555e80044b8a08eb349935939f77e24e3
DESKTOP-7OG5567\dnckd <dnckd942522@gmail.com>
2022-11-02 00:16:04 +0900

    no message

---
+++ components/MealItem.js
@@ -0,0 +1,65 @@
+import { View, Text, Pressable, Image, StyleSheet } from "react-native";
+
+const MealItem = ({ title, imageUrl, duration, complexity, affordability }) => {
+  return (
+    <View style={styles.mealItem}>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={({ pressed }) => {
+          pressed ? styles.buttonPressed : null;
+        }}
+      >
+        <View style={styles.innerContainer}>
+          <View>
+            <Image style={styles.image} source={{ uri: imageUrl }} />
+            <Text style={styles.title}>{title}</Text>
+          </View>
+          <View style={styles.details}>
+            <Text style={styles.detailItem}>{duration}</Text>
+            <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
+            <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
+          </View>
+        </View>
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  mealItem: {
+    margin: 16,
+    borderRadius: 8,
+    overflow: "hidden",
+    backgroundColor: "white",
+    elevation: 4,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  title: {
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 18,
+    margin: 8,
+  },
+  details: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 8,
+    justifyContent: "center",
+  },
+  detailItem: {
+    marginHorizontal: 4,
+    fontSize: 12,
+  },
+  innerContainer: {
+    borderRadius: 8,
+    overflow: "hidden",
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
+});
+
+export default MealItem;
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,12 +1,36 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
 const MealsOverviewScreen = ({ route }) => {
   const { categoryId } = route.params;
+
+  const displayedMeals = MEALS.filter(mealItem => {
+    return mealItem.categoryIds.indexOf(categoryId) >= 0;
+  });
+
+  const renderMealItem = itemData => {
+    const { title, imageUrl, affordability, complexity, duration } =
+      itemData.item;
     
+    return (
+      <MealItem
+        title={title}
+        imageUrl={imageUrl}
+        affordability={affordability}
+        complexity={complexity}
+        duration={duration}
+      />
+    );
+  };
+
   return (
-    <View>
-      <Text>Meals Overview Screen - {categoryId}</Text>
+    <View style={styles.container}>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={item => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 };
